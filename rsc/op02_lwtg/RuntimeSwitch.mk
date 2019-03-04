8aa3ce3b3f94360910d9374bb48cad1f45cac1d0
mdeejay <mdjrussia@gmail.com>
2019-03-04 04:37:27 +0100

    Initial import k39tv1_64_bsp

---
+++ rsc/op02_lwtg/RuntimeSwitch.mk
@@ -0,0 +1,2 @@
+# optional: inherit from base makefile
+include device/mediatek/common/rsc/Op02Base.mk
---
+++ rsc/op09/RuntimeSwitch.mk
@@ -0,0 +1,3 @@
+
+# optional: inherit from base makefile
+include device/mediatek/common/rsc/Op09Base.mk
